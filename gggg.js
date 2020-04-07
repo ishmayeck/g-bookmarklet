90e33ec42f8f393336b01a5ed9634576bcd2f112
Mikhail Novikov <ishmayeck@ishmayeck.com>
2020-04-07 14:42:42 -0400

    gggggg

--- gggg.js
+++ gggg.js
@@ -1,16 +1,16 @@
 javascript:(function() {
 	window.g = function() {
-		function G(element)
+		function G(g)
 		{
-			if (element.childNodes.length > 0) 
-			for (var i = 0; i < element.childNodes.length; i++) {
-			    if(element.childNodes[i].nodeName.toLowerCase() !== 'style' && element.childNodes[i].nodeName.toLowerCase() !== 'script') {
-                    G(element.childNodes[i]);
+			if (g.childNodes.length > 0)
+			for (var i = 0; i < g.childNodes.length; i++) {
+			    if(g.childNodes[i].nodeName.toLowerCase() !== 'style' && g.childNodes[i].nodeName.toLowerCase() !== 'script') {
+                    G(g.childNodes[i]);
                 }
 			}
 
-            if (element.nodeType === Node.TEXT_NODE && element.nodeValue !== '') {
-				element.textContent = element.textContent.replace(/[a-z]/g, 'g').replace(/[A-Z]/g, 'G');
+            if (g.nodeType === Node.TEXT_NODE && g.nodeValue !== '') {
+				g.textContent = g.textContent.replace(/[a-z]/g, 'g').replace(/[A-Z]/g, 'G');
 			}
 		}
 		var html = document.getElementsByTagName('html')[0];
