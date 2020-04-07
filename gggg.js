e32b8e741232646ebd316fec0bcec00b4f9d7871
Mikhail Novikov <ishmayeck@ishmayeck.com>
2020-04-07 14:42:54 -0400

    gggggggg

--- gggg.js
+++ gggg.js
@@ -1,20 +1,20 @@
-javascript:(function() {
-	window.g = function() {
-		function G(g)
-		{
-			if (g.childNodes.length > 0)
-			for (var i = 0; i < g.childNodes.length; i++) {
-			    if(g.childNodes[i].nodeName.toLowerCase() !== 'style' && g.childNodes[i].nodeName.toLowerCase() !== 'script') {
-                    G(g.childNodes[i]);
-                }
-			}
+javascript:(function () {
+  window.g = function () {
+    function G(g) {
+      if (g.childNodes.length > 0)
+        for (var i = 0; i < g.childNodes.length; i++) {
+          if (g.childNodes[i].nodeName.toLowerCase() !== 'style' && g.childNodes[i].nodeName.toLowerCase() !== 'script') {
+            G(g.childNodes[i]);
+          }
+        }
 
-            if (g.nodeType === Node.TEXT_NODE && g.nodeValue !== '') {
-				g.textContent = g.textContent.replace(/[a-z]/g, 'g').replace(/[A-Z]/g, 'G');
-			}
-		}
-		var html = document.getElementsByTagName('html')[0];
-			G(html);
-		};
-	g();
+      if (g.nodeType === Node.TEXT_NODE && g.nodeValue !== '') {
+        g.textContent = g.textContent.replace(/[a-z]/g, 'g').replace(/[A-Z]/g, 'G');
+      }
+    }
+
+    var html = document.getElementsByTagName('html')[0];
+    G(html);
+  };
+  g();
 })();
